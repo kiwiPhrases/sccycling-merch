92296b67073fa8dd60130d16930391d90a552c02
kiwiPhrases <gene.burinsky@gmail.com>
2020-05-22 15:25:02 -0700

    added empty path to urlpatterns

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
+    path(r'', admin.site.urls),
     path('inv_check/', include('inv_check.urls')),
     path('admin/', admin.site.urls),
 ]
