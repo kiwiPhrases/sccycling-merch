06d0a7ab59cf3d2db38c06f928b12c8e535f58a6
kiwiPhrases <gene.burinsky@gmail.com>
2021-05-04 22:32:51 -0700

    updated email relay details

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,4 +156,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'info@usccycling.com'
-EMAIL_HOST_PASSWORD = 'NewTroj@nCycling'
+EMAIL_HOST_PASSWORD = 'waTTs4trojan$'
